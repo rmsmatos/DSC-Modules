690f323d7e64f0fcb2b99f1a8927757a0250366f
rmsmatos <rmsmatos@gmail.com>
2022-06-29 09:13:39 +0100

    new script

---
+++ _Scripts/1-PrepareDisks+Install_AD_Components.ps1
@@ -0,0 +1,86 @@
+﻿configuration ConfigureADNextDC
+{
+    Import-DscResource -ModuleName ActiveDirectoryDsc, StorageDsc, NetworkingDsc, ComputerManagementDSC
+
+    Node localhost
+    {
+        OpticalDiskDriveLetter SetFirstOpticalDiskDriveLetterToZ
+        {
+            DiskId      = 1
+            DriveLetter = 'Z'
+        }
+
+        WaitForDisk Disk2
+        {
+             DiskId = 2
+             RetryIntervalSec = 60
+             RetryCount = 60
+        }
+
+        Disk EVolume
+        {
+             DiskId = 2
+             DriveLetter = 'E'
+             DependsOn = '[WaitForDisk]Disk2'
+        }
+
+        LocalConfigurationManager
+        {
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+        
+        WindowsFeature DNS
+        {
+            Ensure = "Present"
+            Name = "DNS"
+        }
+        
+        WindowsFeature DnsTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature ADTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-Tools"
+            DependsOn = "[WindowsFeature]DNS"
+        }
+        
+        WindowsFeature GPOTools
+        {
+            Ensure = "Present"
+            Name = "GPMC"
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature DFSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-DFS-Mgmt-Con"
+            DependsOn = "[WindowsFeature]DNS"
+        }
+        
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        #
+        # Force the reboot; the automatic reboot stopped working somewhere in 2019... 
+        #
+        PendingReboot RebootAfterInstallingAD
+        {
+            Name = 'RebootAfterInstallingAD'
+            DependsOn = "[xADDomainController]NextDC"
+        }
+    }
+
+}
+
+ConfigureADNextDC -OutputPath C:\DSC\Test
